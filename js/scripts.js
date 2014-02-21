9730f67ed0e79fe70bbc451eb0c3e5ac1fb94292
Alexander Wood & Patti Jurgensen <student@epicodus.com>
2014-02-21 15:45:13 -0800

    uses javascript object

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,15 @@
 var scrabbleScorer = function(wordInput) {
-  var scoreTotal = 0
-  var value1Array = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'];
-  var value2Array = ['D', 'G'];
-  var value3Array = ['B', 'C', 'M', 'P'];
-  var value4Array = ['F', 'H', 'V', 'W', 'Y'];
-  var value5Array = ['K'];
-  var value8Array = ['J', 'X'];
-  var value10Array = ['Q', 'Z'];
-  var letterInput = wordInput.toUpperCase().split('');
+  var stringToCheck = wordInput.toUpperCase();
+  var scoreTotal = 0;
+  var letterValues = {'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1,
+  'R': 1, 'S': 1, 'T': 1, 'D': 2, 'G': 2, 'B': 3, 'C': 3, 'M': 3, 'P': 3, 'F': 4,
+  'H': 4, 'V': 4, 'W': 4, 'Y': 4, 'K': 5, 'J': 8, 'X': 8, 'Q': 10, 'Z': 10};
 
-  for(var i = 0; i < wordInput.length; i++) {
-    if(value1Array.indexOf(letterInput[i]) > -1) {
-      scoreTotal += 1;
-    } else if(value2Array.indexOf(letterInput[i]) > -1) {
-      scoreTotal += 2;
-    } else if(value3Array.indexOf(letterInput[i]) > -1) {
-      scoreTotal += 3;
-    } else if(value4Array.indexOf(letterInput[i]) > -1) {
-      scoreTotal += 4;
-    } else if(value5Array.indexOf(letterInput[i]) > -1) {
-      scoreTotal += 5;
-    } else if(value8Array.indexOf(letterInput[i]) > -1) {
-      scoreTotal += 8;
-    } else if(value10Array.indexOf(letterInput[i]) > -1) {
-      scoreTotal += 10;
-    } else {
-      scoreTotal = "invalid."
-      break;
-      }
-    }
+  for (var i = 0; i < wordInput.length; i++) {
+    scoreTotal = scoreTotal + letterValues[stringToCheck[i]]
+  }
   return scoreTotal;
-};
+}
 
 $(document).ready(function() {
   $("form#scrabble").submit(function(event) {
